77c77db18313998a51a952d41200f57ef872d196
filimonenc <diego.ruslan.rus@gmail.com>
2025-12-02 21:17:25 +0300

    init repo

---
+++ MIDIDeck/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace MIDIDeck.ViewModels;
+
+public abstract class ViewModelBase : ReactiveObject
+{
+}
---
+++ MIDIDeck/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace MIDIDeck.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
